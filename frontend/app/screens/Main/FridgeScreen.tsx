023b09247dc1e94939bc068708ebfe5f64fcc55b
Qczer <119138392+Qczer@users.noreply.github.com>
2025-11-11 21:05:23 +0100

    Fixed Navigation

--- frontend/app/screens/Main/FridgeScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "react-native"
-
-const FridgeScreen = () => {
-    return (
-        <Text>Settings</Text>
-    )
-}
-
-export default FridgeScreen;
--- frontend/app/screens/Main/WelcomeScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "react-native"
-
-const WelcomeScreen = () => {
-    return (
-        <Text>Welcome</Text>
-    )
-}
-
-export default WelcomeScreen;
