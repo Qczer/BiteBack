c2301be6645f4541ca3a7af44c3487bca421bf82
Czareski <kubaucher202@gmail.com>
2025-11-25 19:23:57 +0100

    Improved maps

--- frontend/app/(more)/MapsScreen.tsx
+++ frontend/app/(more)/MapsScreen.tsx
@@ -1,12 +1,14 @@
 import { getPoints, TomTomApiKey } from "@/api/endpoints/dotationpoints";
 import { GreenVar, WhiteVar } from "@/assets/colors/colors";
+import toastConfig from "@/components/ToastConfig";
 import { withCopilotProvider } from "@/components/WithCopilotProvider";
 import { default as translate } from "@/locales/i18n";
 import DotationPoint from "@/types/DotationPoint";
 import { Ionicons } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
+import * as Location from "expo-location";
 import { router, useFocusEffect } from "expo-router";
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -15,11 +17,31 @@ import {
   View,
 } from "react-native";
 import { CopilotStep, useCopilot, walkthroughable } from "react-native-copilot";
+import Toast from "react-native-toast-message";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 
 // Dodajemy style do CopilotView, aby wypełnił ekran
 const CopilotView = walkthroughable(View);
 const CopilotText = walkthroughable(Text);
+
+const showToast = (message: string) => {
+    Toast.show({
+      type: "error",
+      text1: message,
+      position: "top",
+      swipeable: true,
+    });
+  };
+  const showSuccessfulToast = (message: string) => {
+    Toast.show({
+      type: "success",
+      text1: message,
+      position: "top",
+      swipeable: true,
+    });
+  };
+
+
 
 function MapsScreen() {
   const copilot = (key: string) => translate("copilot." + key);
@@ -38,31 +60,90 @@ function MapsScreen() {
   const { start, totalStepsNumber } = useCopilot();
   const hasStartedTutorial = useRef(false);
 
+  useEffect(() => {
+    const initLocation = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return;
+
+      const loc = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
+      const coords: [number, number] = [loc.coords.longitude, loc.coords.latitude];
+
+      setPosition(coords); 
+
+      webviewRef.current?.injectJavaScript(`
+        window.setUserLocationMarker([${coords[0]}, ${coords[1]}], true);
+        true;
+      `);
+    };
+
+    initLocation();
+  }, []);
+  useEffect(() => {
+    let subscriber: Location.LocationSubscription | null = null;
+
+    const startWatching = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return;
+
+      subscriber = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 2000,    
+          distanceInterval: 1,    
+        },
+        (loc) => {
+          const coords: [number, number] = [loc.coords.longitude, loc.coords.latitude];
+
+          setPosition(coords);
+
+          webviewRef.current?.injectJavaScript(`
+            window.setUserLocationMarker([${coords[0]}, ${coords[1]}], false);
+            true;
+          `);
+        }
+      );
+    };
+
+    startWatching();
+
+    return () => subscriber?.remove();
+  }, []);
+
+
+
   const createMarkers = async () => {
     const res = await getPoints(searchText, position, parseInt(distance));
 
+    // Wyczyść markery
     webviewRef.current?.injectJavaScript(`window.clearMarkers(); true;`);
 
     if (res && res.length > 0) {
+      showSuccessfulToast(`Ilość znalezionych punktów dotacji: ${res.length}`)
       const markersData = res.map((p: DotationPoint) => ({
         coords: p.location,
-        name: p.name.replace(/'/g, "\\'").replace(/"/g, '\\"'),
-        description: (p.description || "")
-          .replace(/'/g, "\\'")
-          .replace(/"/g, '\\"'),
+        name: p.name,
+        description: p.description || '',
+        city: p.city,
+        code: p.postalCode,
+        street: p.street,
+        number: p.number,
+        first: res.indexOf(p) == 0
       }));
 
       const jsCode = `
-      (function() {
-        const data = ${JSON.stringify(markersData)};
-        data.forEach(p => window.addMarker(p.coords, p.name, p.description));
+        (function() {
+          const data = ${JSON.stringify(markersData)};
+          data.forEach(p => window.addMarker(p.coords, p.name, p.description, p.city, p.code, p.street, p.number, p.first));
         })();
         true;
-        `;
+      `;
 
       webviewRef.current?.injectJavaScript(jsCode);
+    } else {
+      showToast(`Znaleziono 0 punktów dotacji`)
     }
   };
+
 
   const onMessage = (event: WebViewMessageEvent) => {
     try {
@@ -147,17 +228,43 @@ function MapsScreen() {
             });
         };
 
-        window.addMarker = function(cords, name, description) {
+        window.addMarker = function(cords, name, description, city, code, street, number, first) {
           const popup = new tt.Popup({ offset: 35 })
-              .setHTML(\`<h3>\${name}</h3><p>\${description}</p>\`);
-          const marker = new tt.Marker().setLngLat(cords).setPopup(popup).addTo(map);
+              .setHTML(\`<h3>\${name}</h3><p>\${description}</p><h5> \${city} \${code}, \${street} \${number}</h5>\`);
+
+          const marker = new tt.Marker({color: '#16533f'})
+              .setLngLat(cords)
+              .setPopup(popup)
+              .addTo(map);
+
           markers.push(marker);
+          if (first) {
+            map.flyTo({
+              center: cords,
+              zoom: 12
+            });
+          }
         };
 
-        window.clearMarkers = function () {
-            markers.forEach(m => m.remove())
-            markers = []
-        }
+        let userMarker = null;
+
+        window.setUserLocationMarker = function(coords, flyTo = false) {
+          if (userMarker) userMarker.remove();
+
+          const marker = new tt.Marker({ color: "blue" })
+              .setLngLat(coords)
+              .addTo(map);
+
+          userMarker = marker;
+
+          if (flyTo) {
+            map.flyTo({
+              center: coords,
+              zoom: 14
+            });
+          }
+        };
+
     </script>
 </body>
 </html>`;
@@ -224,6 +331,7 @@ function MapsScreen() {
         domStorageEnabled
         onMessage={onMessage}
       />
+      <Toast config={toastConfig}></Toast>
     </View>
   );
 }
